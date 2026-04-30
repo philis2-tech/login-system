478221aed682ff6f7cae1c40e948f34bc3c222dd
Philis Maruza <philismaruza17@gmail.com>
2026-04-30 16:09:23 +0200

    Fix Google OAuth

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ oauth = OAuth(app)
 
 google = oauth.register(
     name='google',
-    client_id='GOOGLE_CLIENT_ID',
-    client_secret="GOOGLE_CLIENT_SECRET",
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
     access_token_url='https://oauth2.googleapis.com/token',
     authorize_url='https://accounts.google.com/o/oauth2/v2/auth',
     api_base_url='https://www.googleapis.com/oauth2/v2/',
